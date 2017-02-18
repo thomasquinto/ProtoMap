e1dd0ba7182a9f66f4eca1ac8b6afcc5b85d9cb5
Thomas Quinto <thomas@life360.com>
2017-02-18 15:48:59 -0800

    Got rid of transparent status bar since it caused problems with setting map padding

--- app/src/main/java/com/life360/android/protomap/MapActivity.java
+++ app/src/main/java/com/life360/android/protomap/MapActivity.java
@@ -25,14 +25,12 @@ import butterknife.ButterKnife;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
-    private SupportMapFragment mapFragment;
     private LatLng startLocation = new LatLng(59.145833, 10.223611);
     private int mapHeight;
 
     private static final int SLIDE_PANEL_RESTING_HEIGHT_DP = 48;
     private static final float SLIDE_PANEL_ANCHOR_POINT_PERCENTAGE = 0.25f;
     private static final int SLIDE_PANEL_PARALLAX_OFFSET = 500;
-    private static final int STATUS_BAR_COLOR = R.color.primary_main_grape_500;
 
     @BindView(R.id.slide_panel)
     SlidingUpPanelLayout slidePanel;
@@ -48,18 +46,18 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     }
 
     private void setupMapFragment() {
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         // Will asynchronously invoke onMapReady
         mapFragment.getMapAsync(this);
 
-        final ViewTreeObserver viewTreeObserver = mapFragment.getView().getViewTreeObserver();
+        final View view = mapFragment.getView();
+        final ViewTreeObserver viewTreeObserver = view.getViewTreeObserver();
         if (viewTreeObserver.isAlive()) {
             viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                 @Override
                 public void onGlobalLayout() {
-                    mapFragment.getView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                    mapHeight = mapFragment.getView().getHeight() - SLIDE_PANEL_PARALLAX_OFFSET;
-                    System.out.println("MAP HEIGHT: " + mapHeight);
+                    view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    mapHeight = view.getHeight() - SLIDE_PANEL_PARALLAX_OFFSET;
                 }
             });
         }
@@ -79,25 +77,12 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         //slidePanel.setOverlayed(true);
         slidePanel.setCoveredFadeColor(android.R.color.transparent); // disable scroll fading
 
-        setStatusBarTransparent();
-        final AtomicBoolean statusBarState = new AtomicBoolean(false);
         slidePanel.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
             @Override
             public void onPanelSlide(View panel, float slideOffset) {
-                System.out.println("SLIDE OFFSET: " + slideOffset);
-                if (slideOffset == 1.0f) {
-                    if (statusBarState.get()) {
-                        statusBarState.set(false);
-                        setStatusBarColor();
-                    }
-                } else {
-                    if (!statusBarState.get()) {
-                        statusBarState.set(true);
-                        setStatusBarTransparent();
-                    }
+                if (slideOffset < 1.0f) {
                     int paddingBottom = (int) (slideOffset * mapHeight);
                     map.setPadding(0, 0, 0, paddingBottom);
-                    System.out.println("PADDING BOTTOM: " + paddingBottom);
                 }
             }
 
@@ -105,25 +90,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
             }
         });
-
-        //TODO: Set slidePanel top margin programmatically to Status Bar height instead of hard-coded "24dp" in layout
-
-        /* To Turn off transparent status bar effect:
-         * 1) Set panel layout_marginTop to 0dp in map_activity.xml layout resource file
-         * 2) Remove status bar color changes above
-         */
-    }
-
-    private void setStatusBarTransparent() {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-    }
-
-    private void setStatusBarColor() {
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        //TODO: Requires API Level 21
-        window.setStatusBarColor(ContextCompat.getColor(this, STATUS_BAR_COLOR));
     }
 
     private int dpToPixels(int dpValue) {

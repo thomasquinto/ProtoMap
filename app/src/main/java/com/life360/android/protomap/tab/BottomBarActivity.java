7b51b0ded0f0f60db37e7fbbf95319261ea7e92b
Thomas Quinto <thomas@life360.com>
2017-05-30 15:00:01 -0700

    Animated dismissing of tab bar while map is moving

--- app/src/main/java/com/life360/android/protomap/tab/BottomBarActivity.java
+++ app/src/main/java/com/life360/android/protomap/tab/BottomBarActivity.java
@@ -2,11 +2,20 @@ package com.life360.android.protomap.tab;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorCompat;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.view.Surface;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Interpolator;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -31,6 +40,7 @@ public class BottomBarActivity extends FragmentActivity {
 
     BottomBar bottomBar;
 
+    @BindView(R.id.tab1)
     View tab1;
     @BindView(R.id.tab2)
     View tab2;
@@ -70,20 +80,40 @@ public class BottomBarActivity extends FragmentActivity {
         tab3.setVisibility(View.GONE);
         tab4.setVisibility(View.GONE);
 
+        int statusBarColor = 0;
 
         switch (tabId) {
             case R.id.tab_people:
-                if (tab1 != null) tab1.setVisibility(View.VISIBLE);
+                tab1.setVisibility(View.VISIBLE);
+                statusBarColor = R.color.main_blueberry_500;
                 break;
             case R.id.tab_places:
                 tab2.setVisibility(View.VISIBLE);
+                statusBarColor = R.color.main_kale_500;
                 break;
             case R.id.tab_safety:
                 tab3.setVisibility(View.VISIBLE);
+                statusBarColor = R.color.main_fig_500;
                 break;
             case R.id.tab_profile:
                 tab4.setVisibility(View.VISIBLE);
+                statusBarColor = R.color.primary_main_grape_500;
                 break;
+        }
+
+        if (statusBarColor != 0) {
+            if (Build.VERSION.SDK_INT >= 21) {
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.setStatusBarColor(ContextCompat.getColor(this, statusBarColor));
+            }
+            if (getOrientation() == LAND) {
+                if (Build.VERSION.SDK_INT >= 21) {
+                    Window window = getWindow();
+                    window.setNavigationBarColor(ContextCompat.getColor(this, statusBarColor));
+                }
+            }
         }
     }
 
@@ -93,22 +123,58 @@ public class BottomBarActivity extends FragmentActivity {
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                tab1 = mapFragment.getView();
-
                 googleMap.setOnCameraMoveStartedListener(new GoogleMap.OnCameraMoveStartedListener() {
                     @Override
                     public void onCameraMoveStarted(int i) {
-                        bottomBar.setVisibility(View.GONE);
+                        animateOffset(bottomBar, bottomBar.getHeight());
                     }
                 });
 
                 googleMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
                     @Override
                     public void onCameraIdle() {
-                        bottomBar.setVisibility(View.VISIBLE);
+                        animateOffset(bottomBar, 0);
                     }
                 });
             }
         });
     }
+
+    private static final int PORT = 1;
+    private static final int LAND = 2;
+    private int getOrientation() {
+        WindowManager windowService = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+
+        int rotation = windowService.getDefaultDisplay().getRotation();
+
+        if (Surface.ROTATION_0 == rotation) {
+            rotation = PORT;
+        } else if(Surface.ROTATION_180 == rotation) {
+            rotation = PORT;
+        } else if(Surface.ROTATION_90 == rotation) {
+            rotation = LAND;
+        } else if(Surface.ROTATION_270 == rotation) {
+            rotation = LAND;
+        }
+        return rotation;
+    }
+
+    private static final Interpolator INTERPOLATOR = new LinearOutSlowInInterpolator();
+    private ViewPropertyAnimatorCompat mTranslationAnimator;
+
+    private void animateOffset(final View child, final int offset) {
+        ensureOrCancelAnimator(child);
+        mTranslationAnimator.translationY(offset).start();
+    }
+
+    private void ensureOrCancelAnimator(View child) {
+        if (mTranslationAnimator == null) {
+            mTranslationAnimator = ViewCompat.animate(child);
+            mTranslationAnimator.setDuration(300);
+            mTranslationAnimator.setInterpolator(INTERPOLATOR);
+        } else {
+            mTranslationAnimator.cancel();
+        }
+    }
+
 }
